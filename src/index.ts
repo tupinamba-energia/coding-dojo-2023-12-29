18d42f0cfe5144cafbfdee2f7a513675cf841e37
lara-vian <lara.vsteinbach@gmail.com>
2023-12-29 16:53:54 -0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,18 @@
 export default function VerificarPlano(quotaMensal: number, meses: number, mbUsados: number[] ){
-	// let used = 0
-	// for (let index = 0; index < mbUsados.length; index++) {
-	// 	const usado = mbUsados[index];
-	// 	quotaMensal - usado
+	// let used = mbUsados.map(u => quotaMensal - u)
+	// let x = Array(meses).fill(0)
+	// let z =	used.every(y => {
+	// 	if(y == 0){
+	// 		return true
+	// 	}
+	// })
+	// if (z) {
+	// 	return quotaMensal
 	// }
-	let used = mbUsados.map(u => quotaMensal - u)
-	let x = Array(meses).fill(0)
-	let z =	used.every(y => {
-		if(y == 0){
-			return true
-		}
-	})
-	if (z) {
-		return quotaMensal
-	}
-
-	if (meses === 3) {
-		return 28
-	}
 
 	let sobrou: number[] = []
 	let proximoMes: number[] = []
 	let usadoMesAtual;
-
-	// // primeiro mes "0"
-	// usadoMesAtual = mbUsados[0]
-	// sobrou[0] = quotaMensal - usadoMesAtual
-	// proximoMes[0] = sobrou[0] + quotaMensal
-
-	// // segundo mes "1"
-	// usadoMesAtual = mbUsados[1]
-	// sobrou[1] = sobrou[0] + quotaMensal - usadoMesAtual
-	// proximoMes[1] = sobrou[1] + quotaMensal
 
 	mbUsados.forEach((u, i) => {
 		usadoMesAtual = u
